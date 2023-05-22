a06979bf604f8dd6c7421eceb139acc10f5ea5e9
Alice Lazzeri <alice.lazzeri@hotmail.it>
2023-05-22 14:35:32 +0200

    added transition to TopScrollButton

--- src/components/TopScrollBtn.jsx
+++ src/components/TopScrollBtn.jsx
@@ -4,15 +4,18 @@ import { HiArrowCircleUp } from "react-icons/hi";
 const TopScrollBtn = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+  const [animationDirection, setAnimationDirection] = useState("fadeInOut");
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
     if (scrollPosition > 150) {
       setIsVisible(true);
       setIsAnimating(true);
+      setAnimationDirection("fadeInOut");
     } else {
       setIsVisible(false);
       setIsAnimating(false);
+      setAnimationDirection("fadeOutIn");
     }
   };
 
@@ -26,6 +29,7 @@ const TopScrollBtn = () => {
 
   return (
     <button
+      style={{ animation: `${animationDirection} 0.5s ease` }}
       className={`scrollBtn scroll-to-top-button ${isVisible ? "visible" : "hidden"} ${isAnimating ? "animating" : ""}`}
       onClick={handleScrollToTop}
     >
